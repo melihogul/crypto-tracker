c31be7fae563a44b150544417e897ed1101762e6
melihogul <ogul1905@gmail.com>
2025-01-28 16:15:21 +0300

    fix(navbar): resolve image not displayed due to vercel limits

--- components/market-details/coin-info.tsx
+++ components/market-details/coin-info.tsx
@@ -1,94 +1,106 @@
-import { MarketDataType } from "@/types"
-import { format } from "date-fns"
-import Image from "next/image"
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion"
-import { cn } from "@/lib/utils"
-import { ArrowDown, ArrowUp, Medal } from "lucide-react"
+import { MarketDataType } from "@/types";
+import { format } from "date-fns";
+import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
+import { cn } from "@/lib/utils";
+import { ArrowDown, ArrowUp, Medal } from "lucide-react";
 
-const CoinInfo = ({data}: {data: MarketDataType}) => {
+const CoinInfo = ({ data }: { data: MarketDataType }) => {
   return (
     <div className="select-none">
-        <div className="text-lg font-semibold leading-none tracking-tight">
-            Coin Details
-            <div className="flex gap-x-1 text-xs mt-1 text-muted-foreground">
-            <p className="">Last Update:</p>
-            <div className="flex gap-x-1">
-                <p>{format(data.last_updated, "p")}</p> -
-                <p>{format(data.last_updated, "P")}</p>
-            </div>
-            </div>
+      <div className="text-lg font-semibold leading-none tracking-tight">
+        Coin Details
+        <div className="flex gap-x-1 text-xs mt-1 text-muted-foreground">
+          <p className="">Last Update:</p>
+          <div className="flex gap-x-1">
+            <p>{format(data.last_updated, "p")}</p> -
+            <p>{format(data.last_updated, "P")}</p>
+          </div>
         </div>
-        <div className="mt-3 text-sm text-muted-foreground">
-            <div className="flex gap-x-1">
-                <p className="font-bold select-none">Name:</p>
-                <Image alt="img" src={data.image} height={20} width={20} />
-                {data.name}
-                <kbd className="h-5 items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground opacity-100">
-                    {data.symbol.toUpperCase()}
-                </kbd>
-            </div>
-            <div className="flex gap-x-1">
-                <p className="font-bold select-none">Current Price:</p>
-                ${data.current_price}
-            </div>
-            <div className="flex gap-x-1">
-                <p className="font-bold select-none">24H Price Change:</p>
-                <p className={cn(
-                      data.price_change_percentage_24h.toLocaleString().startsWith("-")
-                      ? "text-rose-500"
-                      : "text-emerald-500"
-                )}>
-                    {data.price_change_percentage_24h}%
-                </p>
-            </div>
-            <div className="flex gap-x-1 mt-2">
-                <p className="flex text-center justify-center items-center font-bold select-none">
-                    <ArrowUp className="w-4 h-4" />
-                    24H High:
-                </p>
-                ${data.high_24h}
-            </div>
-            <div className="flex gap-x-1">
-                <p className="flex text-center justify-center items-center font-bold select-none">
-                    <ArrowDown className="w-4 h-4" />
-                    24H Low:
-                </p>
-                ${data.low_24h}
-            </div>
-            <div className="flex gap-x-1 mt-3">
-                <p className="font-bold select-none">All Time High:</p>
-                ${data.ath}
-            </div>
-            <div className="flex gap-x-1 mt-2">
-                <Accordion type="single" collapsible>
-                    <AccordionItem value="item-1">
-                        <AccordionTrigger>Other Details</AccordionTrigger>
-                        <AccordionContent>
-                        <div className="flex gap-x-1 select-none">
-                            <p className="font-semibold">Market Cap:</p>
-                            ${data.market_cap}
-                        </div>
-                        <div className="flex gap-x-1 select-none">
-                            <p className="font-semibold">24H Market Cap Change:</p>
-                            <p className={cn(
-                                data.market_cap_change_percentage_24h.toLocaleString().startsWith("-")
-                                ? "text-rose-500"
-                                : "text-emerald-500"
-                            )}>
-                                {data.market_cap_change_percentage_24h}%
-                            </p>
-                        </div>
-                        <div className="flex text-center gap-x-1 select-none">
-                            <p className="font-semibold">Market Cap Rank:</p>
-                            <p className="flex items-center justify-center gap-x-1">{data.market_cap_rank}<Medal className="h-4 w-4" /></p>
-                        </div>
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
-            </div>
+      </div>
+      <div className="mt-3 text-sm text-muted-foreground">
+        <div className="flex gap-x-1">
+          <p className="font-bold select-none">Name:</p>
+          <Image alt={data.name} src={data.image} height={20} width={20} />
+          {data.name}
+          <kbd className="h-5 items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs font-medium text-muted-foreground opacity-100">
+            {data.symbol.toUpperCase()}
+          </kbd>
         </div>
+        <div className="flex gap-x-1">
+          <p className="font-bold select-none">Current Price:</p>$
+          {data.current_price}
+        </div>
+        <div className="flex gap-x-1">
+          <p className="font-bold select-none">24H Price Change:</p>
+          <p
+            className={cn(
+              data.price_change_percentage_24h.toLocaleString().startsWith("-")
+                ? "text-rose-500"
+                : "text-emerald-500"
+            )}
+          >
+            {data.price_change_percentage_24h}%
+          </p>
+        </div>
+        <div className="flex gap-x-1 mt-2">
+          <p className="flex text-center justify-center items-center font-bold select-none">
+            <ArrowUp className="w-4 h-4" />
+            24H High:
+          </p>
+          ${data.high_24h}
+        </div>
+        <div className="flex gap-x-1">
+          <p className="flex text-center justify-center items-center font-bold select-none">
+            <ArrowDown className="w-4 h-4" />
+            24H Low:
+          </p>
+          ${data.low_24h}
+        </div>
+        <div className="flex gap-x-1 mt-3">
+          <p className="font-bold select-none">All Time High:</p>${data.ath}
+        </div>
+        <div className="flex gap-x-1 mt-2">
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Other Details</AccordionTrigger>
+              <AccordionContent>
+                <div className="flex gap-x-1 select-none">
+                  <p className="font-semibold">Market Cap:</p>${data.market_cap}
+                </div>
+                <div className="flex gap-x-1 select-none">
+                  <p className="font-semibold">24H Market Cap Change:</p>
+                  <p
+                    className={cn(
+                      data.market_cap_change_percentage_24h
+                        .toLocaleString()
+                        .startsWith("-")
+                        ? "text-rose-500"
+                        : "text-emerald-500"
+                    )}
+                  >
+                    {data.market_cap_change_percentage_24h}%
+                  </p>
+                </div>
+                <div className="flex text-center gap-x-1 select-none">
+                  <p className="font-semibold">Market Cap Rank:</p>
+                  <p className="flex items-center justify-center gap-x-1">
+                    {data.market_cap_rank}
+                    <Medal className="h-4 w-4" />
+                  </p>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CoinInfo
+export default CoinInfo;
--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -4,33 +4,30 @@ import Image from "next/image";
 import Link from "next/link";
 
 const font = Poppins({
-    subsets: ["latin"],
-    weight: ["200","300","400","500","600","700","800"]
-})
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
 
 export const Logo = () => {
-    return(
-        <Link href={"/"}>
-            <div className="pl-5 sm:pl-20 pr-10 sm: flex items-center gap-x-4 hover:opacity-75 transition">
-                <div className="p-1 lg:mr-0 shrink-0">
-                    <Image
-                    src={"/wallet.png"}
-                    alt="Gamehub"
-                    height={"32"}
-                    width={"32"}
-                     />
-                </div>
-                <div className={cn(
-                    font.className,
-                    )}>
-                    <p className="text-lg font-semibold">
-                        Crypto Tracker
-                    </p>
-                    <p className="text-xs text-[#4b8088] hidden sm:block">
-                        Know your calculations, realize your profits
-                    </p>
-                </div>
-            </div>
-        </Link>
-    )
-}
+  return (
+    <Link href={"/"}>
+      <div className="pl-5 sm:pl-20 pr-10 sm: flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="p-1 lg:mr-0 shrink-0">
+          <Image
+            src={"/wallet.png"}
+            alt="Crypto Tracker"
+            height={32}
+            width={32}
+            unoptimized
+          />
+        </div>
+        <div className={cn(font.className)}>
+          <p className="text-lg font-semibold">Crypto Tracker</p>
+          <p className="text-xs text-[#4b8088] hidden sm:block">
+            Know your calculations, realize your profits
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
